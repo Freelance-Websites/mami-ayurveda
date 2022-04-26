807a5a6daaf15ca01802375bb0e8a741c5a9758a
Juan Martín García <hello@juangarcia.com.ar>
2022-04-26 16:53:20 -0300

    Move script component outside head component

--- components/MainHead.js
+++ components/MainHead.js
@@ -3,15 +3,15 @@ import Script from 'next/script';
 
 export default function MainHead() {
   return (
-    <Head>
-      {/* Title */}
-      <title>Mami Ayurveda</title>
-      
-      {/* Responsivenes */}
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-
-      {/* Scripts */}
+    <>
+      <Head>
+        {/* Title */}
+        <title>Mami Ayurveda</title>
+        
+        {/* Responsivenes */}
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
       <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-    </Head>
+    </>
   );
 }
